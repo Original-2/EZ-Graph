7ef55c213cb515b239949590ea1d7c374ebc29c7
Dom-DJ <61599006+Dom-DJ@users.noreply.github.com>
2021-11-15 08:03:04 +0000

    Commenting code, makes it easier to read

--- fit.py
+++ fit.py
@@ -1,21 +1,21 @@
-import gi
+import gi # used for GTK import
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk # used for gtk elements
 
 class TrainWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="Make Model")
-        self.values_list = {"optimizer":None, "loss":None, "metrics":None}
+        self.values_list = {"optimizer":None, "loss":None, "metrics":None} # dict where values are stored
 
-        self.box = Gtk.Box(spacing=6)
+        self.box = Gtk.Box(spacing=6) # box for the rest of the content to be contained in
         self.add(self.box)
 
-        listbox = Gtk.ListBox()
+        listbox = Gtk.ListBox() # left hand column, this contains all of the options for the current layer type selected
         listbox.set_selection_mode(Gtk.SelectionMode.NONE)
         self.box.pack_start(listbox, True, True, 0)
 
-        row = Gtk.ListBoxRow()
+        row = Gtk.ListBoxRow() # area where batch size can be changed
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         row.add(hbox)
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
@@ -27,7 +27,7 @@ class TrainWindow(Gtk.Window):
         hbox.pack_start(self.BatchSize, False, True, 0)
         listbox.add(row)
 
-        row = Gtk.ListBoxRow()
+        row = Gtk.ListBoxRow() # area where epoch number can be changed
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         row.add(hbox)
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
@@ -39,7 +39,7 @@ class TrainWindow(Gtk.Window):
         hbox.pack_start(self.EpochNum, False, True, 0)
         listbox.add(row)
 
-        row = Gtk.ListBoxRow()
+        row = Gtk.ListBoxRow() # area where validation split can be changed
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         row.add(hbox)
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
@@ -51,7 +51,7 @@ class TrainWindow(Gtk.Window):
         hbox.pack_start(self.ValSplit, False, True, 0)
         listbox.add(row)
 
-        row = Gtk.ListBoxRow()
+        row = Gtk.ListBoxRow() # shows a progress bar, line or nothing when running script
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         row.add(hbox)
         label = Gtk.Label(label="verbose (visualisation of epochs)", xalign=0)
@@ -63,11 +63,11 @@ class TrainWindow(Gtk.Window):
         hbox.pack_start(self.verbose, False, True, 0)
         listbox.add(row)
 
-        buttonbox = Gtk.ListBox()
+        buttonbox = Gtk.ListBox() # last row
         buttonbox.set_selection_mode(Gtk.SelectionMode.NONE)
         self.box.pack_start(buttonbox, True, True, 0)
 
-        row = Gtk.ListBoxRow()
+        row = Gtk.ListBoxRow() # sav the info on the training
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=200)
         row.add(hbox)
         self.button1 = Gtk.Button(label="Save")
@@ -75,12 +75,12 @@ class TrainWindow(Gtk.Window):
         hbox.pack_start(self.button1, True, True, 0)
         buttonbox.add(row)
 
-    def on_button2_clicked(self, widget):
+    def on_button2_clicked(self, widget): # saves model
         program = f""
 
         program += f"model.fit("
 
-        batch = self.BatchSize.get_text()
+        batch = self.BatchSize.get_text() # get variables from text boxes above
         epoch = self.EpochNum.get_text()
         split = self.ValSplit.get_text()
 
@@ -109,5 +109,5 @@ class TrainWindow(Gtk.Window):
 
         program += f"x=x, y=y, batch_size={batch}, epochs={epoch}, validation_split={split}, verbose={verbose})"
 
-        with open("train.txt", 'w') as f:
+        with open("train.txt", 'w') as f: # writes info to text box
             f.write(program)
